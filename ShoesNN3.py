40b9a844e213d581b3c9b971e11ecc76dc128ae4
yeere91 <regannn@gmail.com>
2016-12-15 16:57:14 -0500

    redoing ShoesNN3

--- ShoesNN3.py
+++ ShoesNN3.py
@@ -1,40 +1,3 @@
-'''This script goes along the blog post
-"Building powerful image classification models using very little data"
-from blog.keras.io.
-It uses data that can be downloaded at:
-https://www.kaggle.com/c/dogs-vs-cats/data
-In our setup, we:
-- created a data/ folder
-- created train/ and validation/ subfolders inside data/
-- created cats/ and dogs/ subfolders inside train/ and validation/
-- put the cat pictures index 0-999 in data/train/cats
-- put the cat pictures index 1000-1400 in data/validation/cats
-- put the dogs pictures index 12500-13499 in data/train/dogs
-- put the dog pictures index 13500-13900 in data/validation/dogs
-So that we have 1000 training examples for each class, and 400 validation examples for each class.
-In summary, this is our directory structure:
-```
-data/
-    train/
-        dogs/
-            dog001.jpg
-            dog002.jpg
-            ...
-        cats/
-            cat001.jpg
-            cat002.jpg
-            ...
-    validation/
-        dogs/
-            dog001.jpg
-            dog002.jpg
-            ...
-        cats/
-            cat001.jpg
-            cat002.jpg
-            ...
-```
-'''
 import os
 import h5py
 import numpy as np
@@ -54,7 +17,7 @@ train_data_dir = 'classes/train'
 validation_data_dir = 'classes/validation'
 nb_train_samples = 3000
 nb_validation_samples = 1160
-nb_epoch = 100
+nb_epoch = 50
 
 
 datagen = ImageDataGenerator(rescale=1./255)
@@ -112,62 +75,50 @@ for k in range(f.attrs['nb_layers']):
     weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
     model.layers[k].set_weights(weights)
 f.close()
-print('VGG16 Model loaded.')
 
-# prepare data augmentation configuration
-train_datagen = ImageDataGenerator(
-        rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
+print '-'*50
+print('VGG16 Weights loaded.')
+print '-'*50
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+generator = datagen.flow_from_directory(
+            train_data_dir,
+            target_size=(img_width, img_height),
+            batch_size=32,
+            class_mode=None,
+            shuffle=False)
+    bottleneck_features_train = model.predict_generator(generator, nb_train_samples)
+    np.save(open('bottleneck_features_train.npy', 'w'), bottleneck_features_train)
 
-train_generator = train_datagen.flow_from_directory(
-        train_data_dir,
-        target_size=(img_height, img_width),
-        batch_size=32,
-        class_mode='binary')
-
-validation_generator = test_datagen.flow_from_directory(
+generator = datagen.flow_from_directory(
         validation_data_dir,
-        target_size=(img_height, img_width),
+        target_size=(img_width, img_height),
         batch_size=32,
-        class_mode='binary')
+        class_mode=None,
+        shuffle=False)
+bottleneck_features_validation = model.predict_generator(generator, nb_validation_samples)
+np.save(open('bottleneck_features_validation.npy', 'w'), bottleneck_features_validation)
 
-top_model = Sequential()
-top_model.add(Flatten(input_shape=model.output_shape[1:]))
-top_model.add(Dense(256, activation='relu'))
-top_model.add(Dropout(0.5))
-top_model.add(Dense(1, activation='sigmoid'))
+print '-'*50
+print ('Dataset trained on VGG16!')
+print '-'*50
 
-# note that it is necessary to start with a fully-trained
-# classifier, including the top classifier,
-# in order to successfully do fine-tuning
-top_model.load_weights(top_model_weights_path)
+train_data = np.load(open('bottleneck_features_train.npy'))
+train_labels = np.array([0] * (nb_train_samples / 2) + [1] * (nb_train_samples / 2))
 
-# add the model on top of the convolutional base
-model.add(top_model)
+validation_data = np.load(open('bottleneck_features_validation.npy'))
+validation_labels = np.array([0] * (nb_validation_samples / 2) + [1] * (nb_validation_samples / 2))
+
+my_model = Sequential()
+my_model.add(Flatten(input_shape=train_data.shape[1:]))
+my_model.add(Dense(256, activation='relu'))
+my_model.add(Dropout(0.5))
+my_model.add(Dense(1, activation='sigmoid'))
+
+my_model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
 print ("Transfer Training Complete")
 
-# set the first 25 layers (up to the last conv block)
-# to non-trainable (weights will not be updated)
-for layer in model.layers[:25]:
-    layer.trainable = False
-
-# compile the model with a SGD/momentum optimizer
-# and a very slow learning rate.
-model.compile(loss='binary_crossentropy',
-              optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
-              metrics=['accuracy'])
-
-# fine-tune the model
-model.fit_generator(
-        train_generator,
-        samples_per_epoch=nb_train_samples,
-        nb_epoch=nb_epoch,
-        validation_data=validation_generator,
-        nb_val_samples=nb_validation_samples)
-
+model.fit(train_data, train_labels,
+              nb_epoch=nb_epoch, batch_size=32,
+              validation_data=(validation_data, validation_labels))
 model.save_weights(top_model_weights_path)
